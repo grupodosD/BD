1167bec0a7b6ea76db6c1c718c9fe64bb89edfa2
grupodosD <164226815+grupodosD@users.noreply.github.com>
2024-10-12 16:03:38 -0300

    Add files via upload

--- HotelM.sql
+++ HotelM.sql
@@ -2,15 +2,17 @@ CREATE DATABASE HotelM;
 
 USE HotelM;
 
+-- Criar a tabela Cliente com criptografia para a coluna Senha
 CREATE TABLE Cliente (
     Id_Cliente INT PRIMARY KEY NOT NULL auto_increment,
     Nome VARCHAR(20),
-    Cpf BIGINT,
+    Cpf char (11) UNIQUE NOT NULL,
     Endereco VARCHAR(20),
     Senha VARBINARY(255),
-    Telefone BIGINT
+    Telefone CHAR (11)
 );
 
+-- Criar a tabela Reserva
 CREATE TABLE Reserva (
     Id_Reserva INT PRIMARY KEY NOT NULL auto_increment,
     Id_Cliente INT,
@@ -18,23 +20,26 @@ CREATE TABLE Reserva (
     Data_Checkout DATE,
     Status_Reserva VARCHAR(20),
     FOREIGN KEY (Id_Cliente) REFERENCES Cliente(Id_Cliente)
+    on update cascade
 );
 
 
+-- Criar a tabela Tipos_Quartos
+CREATE TABLE Tipos_Quartos (
+    Id_TipoQuarto INT PRIMARY KEY NOT NULL auto_increment,
+    Suite VARCHAR(40)
+);
+
+-- Criar a tabela Quarto
 CREATE TABLE Quarto (
     Id_Quarto INT PRIMARY KEY NOT NULL auto_increment,
-    Tipos_Quartos VARCHAR(20),
+    Id_TipoQuarto INT,
     Status_Quartos VARCHAR(20),
     Id_Cliente INT,
     Id_Reserva INT,
     FOREIGN KEY (Id_Cliente) REFERENCES Cliente(Id_Cliente),
-    FOREIGN KEY (Id_Reserva) REFERENCES Reserva(Id_Reserva)
-);
-
-
-CREATE TABLE Tipos_Quartos (
-    Id_TipoQuarto INT PRIMARY KEY NOT NULL auto_increment,
-    Suite VARCHAR(40)
+    FOREIGN KEY (Id_Reserva) REFERENCES Reserva(Id_Reserva),
+    FOREIGN KEY (Id_TipoQuarto) REFERENCES Tipos_Quartos(Id_TipoQuarto)
 );
 
 -- Criar a tabela Pagamentos
@@ -48,6 +53,7 @@ CREATE TABLE Pagamentos (
     Status_Pagamento VARCHAR(20),
     FOREIGN KEY (Id_Cliente) REFERENCES Cliente(Id_Cliente),
     FOREIGN KEY (Id_Reserva) REFERENCES Reserva(Id_Reserva)
+	on update cascade
 );
 
 -- Criar a tabela Cargo
@@ -60,10 +66,16 @@ CREATE TABLE Cargo (
 CREATE TABLE Funcionario (
     Id_funcionario INT PRIMARY KEY NOT NULL auto_increment,
     Id_Cargo INT,
-    Nome VARCHAR(20),
-    Cargo VARCHAR(20),
+    Nome VARCHAR(20) NOT NULL,
+    Senha VARBINARY(255) NOT NULL,
+    Nacimento DATE NOT NULL,
+    Telefone char (11) UNIQUE NOT NULL,
+    Cpf char (11) UNIQUE NOT NULL,
+    Cargo VARCHAR(20) NOT NULL,
+    Efetivado DATE,
     Salario DECIMAL(10,2),
     FOREIGN KEY (Id_Cargo) REFERENCES Cargo(Id_Cargo)
+	on update cascade
 );
 
 -- Criar a tabela Servico
@@ -74,30 +86,38 @@ CREATE TABLE Servico (
     Descricao VARCHAR(20),
     Preco DECIMAL(10,2),
     FOREIGN KEY (Id_Funcionario) REFERENCES Funcionario(Id_funcionario)
+	on update cascade
+);
+
+	CREATE TABLE Login (
+	Id_usuario INT PRIMARY KEY NOT NULL auto_increment, 
+	Nome_usuario VARCHAR (20) NOT NULL,
+	Email_usuario varchar (20) UNIQUE NOT NULL,
+    Senha_usuario VARCHAR (20) UNIQUE NOT NULL
 );
 
 
 -- Exemplo de inserção de dados criptografados
 -- a chave de criptografia segura e secreta
+
 SET @chave_secreta = UNHEX('526F79616C506C616365000000000000');
 
--- Inserindo dados na tabela Cliente com a senha criptografada
-INSERT INTO Cliente (Id_Cliente, Nome, Cpf, Endereco, Senha, Telefone)
-VALUES (
-    'João da Silva',
-    123456789,
-    'Rua Exemplo, 123',
-    AES_ENCRYPT('senha123', @chave_secreta), -- Criptografar senha
-    987654321
+
+INSERT INTO Cargo ( NomeCargo) values (
+'testador'
 );
 
-INSERT INTO Cliente (Nome, Cpf, Endereco, Senha, Telefone)
+INSERT INTO Funcionario (Id_Cargo, Nome, Senha, Nacimento, Telefone, Cpf, Cargo, Efetivado, Salario)
 VALUES (
-    'bombom@gmail.com',
-    924456779,
-    'Rua Exemplo, 890',
-    AES_ENCRYPT('mysqlamo', @chave_secreta), -- Criptografar senha
-    987654321
+    1,
+    'Bombom',
+    AES_ENCRYPT('amojuju', @chave_secreta), -- Criptografar senha
+    '1990-01-01',
+    987654321,
+    123456789,
+    'testador',
+    '2020-01-01',
+    5000.00
 );
 
 
